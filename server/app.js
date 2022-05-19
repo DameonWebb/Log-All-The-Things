9a877215c23aebad5a3b1a3f04d2c793e2d8398c
DameonWebb <dameonlwebb@gmail.com>
2022-05-19 15:27:51 -0700

    middleware is responding

--- server/app.js
+++ server/app.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const morgan = require('morgan');
 const fs = require('fs');
 const app = express();
 const csvFilePath = './server/log.csv'
 const csv = require('csvtojson')
-let log = "";
-//var method = req;
 
+app.use(morgan('dev'))
+app.set('json spaces', 2)
+
+app.get('/', (req, res) => {
+    let Agent = req.headers['user-agent'].replace(/,/g, "");
+    let Time = new Date().toISOString();
+    let Method = req.method;
+    let Resource = req.originalUrl;
+    let Version = "HTTP/" + req.httpVersion;
+    let Status = res.statusCode;
+
+    let allData = `${Agent},${Time},${Method},${Resource},${Version},${Status}`; 
+
+    fs.appendFile('./server/log.csv', '\n' + allData, function() {
+        console.log("File was appended!");
+    });  
+
+    res.status(200);
+   
+})
+
+
+    app.get('/logs', (req, res) => {
+        csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj)=>{
+            console.log(jsonObj);
+            res.json(jsonObj);
+        })
+    });
+
+
+
+/*
 //middleware
     app.use((req, res, next) => {
         //handle the user agent
@@ -26,10 +59,21 @@ let log = "";
         }
     }?
 
+    // resource method to log path and the file requested
+    app.use(function (req, res, next) {
+        var filename = path.basename(req.url);
+        var extension = path.extname(filename);
+        if (extension === 'index.html' || './')
+            console.log("The file " + filename + " was requested.");
+        next();
+    }):
+
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
-
-}):
+    if(req == '/') {
+       console.log('ok');
+    }
+});
 
 app.get('/logs', (req, res) => {
     csv()
@@ -39,5 +83,9 @@ app.get('/logs', (req, res) => {
     })
 });
 
+*/
+
 module.exports = app;
+
+
 
